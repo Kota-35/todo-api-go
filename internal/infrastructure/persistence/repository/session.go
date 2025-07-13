a3e626dd79d9be72e4861b3a33088fae2418cb20
Kota-35 <ishikuro6.2@gmail.com>
2025-07-13 17:25:34 +0900

    feat: セッションリポジトリの実装
    - セッションの保存とトークンによる検索機能を持つセッションリポジトリを新規作成。
    - 新しいセッションの作成時にエラーハンドリングを追加し、セッションの復元機能を実装。

---
+++ internal/infrastructure/persistence/repository/session.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"todo-api-go/internal/domain/entity"
+	"todo-api-go/internal/domain/repository"
+	valueobject "todo-api-go/internal/domain/valueobject"
+	"todo-api-go/pkg/database"
+	"todo-api-go/prisma/db"
+)
+
+type sessionRepository struct {
+	client *db.PrismaClient
+}
+
+func NewSessionRepository() repository.SessionRepository {
+	return &sessionRepository{
+		client: database.PrismaClient,
+	}
+}
+
+func (s *sessionRepository) Save(session *entity.Session) error {
+	ctx := context.Background()
+
+	if session.IsNew() {
+		// 新規作成
+		createdSession, err := s.client.Session.CreateOne(
+			db.Session.TokenHash.Set(session.TokenHash()),
+			db.Session.ExpiresAt.Set(session.ExpiresAt()),
+			db.Session.User.Link(
+				db.User.ID.Equals(session.UserId()),
+			),
+		).Exec(ctx)
+
+		if err != nil {
+			return fmt.Errorf("[sessionRepository]セッションの作成に失敗しました: %w", err)
+		}
+
+		return session.SetID(createdSession.ID)
+	} else {
+		return fmt.Errorf("[sessionRepository]セッションの更新はできません")
+	}
+}
+
+func (s *sessionRepository) FindByToken(token *valueobject.RefreshToken) (*entity.Session, error) {
+	ctx := context.Background()
+
+	session, err := s.client.Session.FindUnique(
+		db.Session.TokenHash.Equals(token.Hash()),
+	).Exec(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("[sessionRepository]セッションの検索に失敗しました: %w", err)
+	}
+
+	// domain entityに変換
+	restoredSession := entity.ReconstructSession(
+		session.ID,
+		session.UserID,
+		session.TokenHash,
+		session.ExpiresAt,
+		session.CreatedAt,
+		session.IsRevoked,
+	)
+
+	return &restoredSession, nil
+}
