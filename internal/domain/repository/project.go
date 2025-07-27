74da606c0d67132836a35b3ac415d82fdef46d22
Kota-35 <ishikuro6.2@gmail.com>
2025-07-27 22:49:41 +0900

    feat: ProjectRepositoryにチームIDでプロジェクトを取得するメソッドを追加

--- internal/domain/repository/project.go
+++ internal/domain/repository/project.go
@@ -6,4 +6,5 @@ type ProjectRepository interface {
 	Save(project *entity.Project) error
 	FindByID(id string) (*entity.Project, error)
 	// FindByOwnerID(ownerId string) (*entity.Project, error)
+	FindProjectsByTeamID(teamId string) ([]entity.Project, error)
 }
